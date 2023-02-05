9b708b47d5cd97f3c46060b5592771e7bea95ea9
bearsyankees <bearsyankees@gmail.com>
2023-02-05 15:40:13 -0500

    accounts for nickname change

--- main.py
+++ main.py
@@ -18,17 +18,28 @@ groupme_client = Client.from_token(os.environ.get("TOKEN"))
 
 nu = groupme_client.groups.get("91997574")
 
-message_page = nu.messages.list()
+id_to_member = {} # so changing nicknames doesn't matter
+for member in nu.members:
+    id_to_member[member.user_id] = member.nickname
+print(id_to_member)
+
 #print(sorted(nu.messages.list_all(),key = lambda x: len(x.favorited_by), reverse = True))
 def main():
     #TODO skip this step just use gpsread
     with open("out.csv", "w") as f:
         writer = csv.writer(f)
+        message_page = nu.messages.list()
         for message in nu.messages.list_all():
-           #print(dir(message))
-           writer.writerow([message.created_at, message.name,message.text,len(message.favorited_by), len(message.attachments)])
+            try:
+                name = id_to_member[message.data["sender_id"]] # to account for change in names
+            except KeyError:
+                name = message.data["sender_id"]
+            writer.writerow([message.created_at, name,message.text,len(message.favorited_by), len(message.attachments)])
 
     with open('out.csv', 'r') as file_obj:
         content = file_obj.read()
         client.import_csv(spreadsheet.id, data=content)
+
+if __name__ == "__main__":
+    main()
 
